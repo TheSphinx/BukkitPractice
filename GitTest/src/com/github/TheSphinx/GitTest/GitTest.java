9192788c3d778af194da779f46f83e50bb8ded1a
TheSphinx <jcurtis0320@gmail.com>
2012-03-04 17:39:22 -0500

    Test2

---
+++ GitTest/src/com/github/TheSphinx/GitTest/GitTest.java
@@ -0,0 +1,12 @@
+package com.github.TheSphinx.GitTest;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("This is just a test.");
+	}
+}
